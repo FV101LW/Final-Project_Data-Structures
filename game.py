c1b19f14389b052d93af9d74a3be964be0f92438
FV101LW <limweim48@gmail.com>
2024-11-28 00:36:19 +0800

    Update game.py
    Created main func.

--- game.py
+++ game.py
@@ -73,3 +73,66 @@ quit()
 #Main
 while True:
   
+for event in pygame.event.get():
+  if event.type == pygame.KEYDOWN:
+    if event.key == pygame.K_UP:
+      change_to = 'UP'
+      if event.key == pygame.K_DOWN:
+        change_to = 'DOWN'
+        if event.key == pygame.K_LEFT:
+          change_to = 'LEFT'
+          if event.key == pygame.K_RIGHT:
+            change_to = 'RIGHT'
+
+#Prevent snake from moving two directions simultaneously
+if change_to == 'UP' and direction != 'DOWN':
+  direction == 'UP
+if change_to == 'DOWN' and direction != 'UP':
+  direction == 'DOWN'
+if change_to == 'LEFT' and direction != 'RIGHT':
+  direction == 'LEFT'
+if change_to == 'RIGHT' and direction != 'LEFT:
+  direction == 'RIGHT'
+
+#Moving snake
+if direction == 'UP':
+  snake_position[1] -= 10
+if direction == 'DOWN':
+  snake_position[1] += 10
+if direction == 'LEFT':
+  snake_position[0] -= 10
+if direction == 'RIGHT':
+  snake_position[0] += 10
+
+#Snake body growth mechanism
+#Incrementation by 10
+snake_body.insert(0, list(snake_position))
+if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
+  score += 10
+  fruit_spawn = False
+else:
+  snake_body.pop()
+
+if not fruit_spawn:
+  fruit_position = [random.randrange(1, (window_x//10)) * 10, random.randrange(1, (window_y//10)) * 10]
+
+fruit_spawn = True
+game_spawn.fill(black)
+
+for pos in snake_body: 
+  pygame.draw_rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
+  pygame.draw_rect(game_window, white, pygame.Rect(fruit_position[0], fruit_position[1], 10, 10))
+
+#Game Over condition
+if snake_position[0] < 0 or snake_position[0] > window_x-10:
+  game_over()
+if snake_position[1] < 0 or snake_position[1] > window_y-10:
+  game_over()
+
+for block in snake_body[1:]:
+  if snake_position[0] == block[0] and snake_position[1] == block[1]: game_over()
+
+show_score(1, white, 'times new roman', 20)
+
+pygame.display.update()
+
