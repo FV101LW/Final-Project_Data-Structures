4e44b5a710ec7eeb871d39f7064bd2c2bf5c738e
FV101LW <limweim48@gmail.com>
2024-12-19 17:14:46 +0800

    Update game.py

--- game.py
+++ game.py
@@ -4,15 +4,15 @@ import time
 
 snake_speed = 15
 
-windox_x = 720
-windox_y = 800
+window_x = 720
+windox=w_y = 800
 
 #colors
 black = pygame.Color(0, 0, 0)
-white = pygame.color(255, 255, 255)
-red = pygame.color(255, 0, 0)
-green = pygame.color(0, 255, 0)
-blue = pygame.color(0, 0, 255)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
 
 pygame.init() #initialization of pygame
 
@@ -37,38 +37,20 @@ change_to = direction
 score = 0
 
 def show_score(choice, color, font, size):
-
-  score_font = pygame.font.SysFont(font, size)
-
+score_font = pygame.font.SysFont(font, size)
 score_surface = score_font.render('Score: ' + str(score), True, color)
-
 score_rect = score_surface.get_rect()
-
 game_window.blit(score_surface, score_rect)
 
 def game_over():
-
-  my_font = pygame.font.SysFont('times new roman', 50)
-
-game_over_surface = my_font.render('Your Score is: ' + str(score), True, red)
-
-#create rectangular object for text
-game_over_rect = game_over_surface.get_rect()
-
-#set position of text
-game_over_rect.midtop = (window_x/2, window_y/4)
-
-#draw text on screen
-game_window.blit(game_over_surface, game_over_rect)
-
-#program quits after 2 secs.
-time.sleep(2)
-
-#pygame library deactivation
-pygame.quit()
-
-#quit program
-quit()
+    my_font = pygame.font.SysFont('times new roman', 50)
+    game_over_surface = my_font.render('Your Score is: ' + str(score), True, red)
+    game_over_rect = game_over_surface.get_rect() #create rectangular object for text
+    game_over_rect.midtop = (window_x/2, window_y/4) #set position of text
+    game_window.blit(game_over_surface, game_over_rect) #draw text on screen
+    time.sleep(2) #program quits after 2 secs.
+    pygame.quit() #pygame library deactivation
+    quit() #quit program
 
 #Main
 while True:
@@ -77,22 +59,22 @@ for event in pygame.event.get():
   if event.type == pygame.KEYDOWN:
     if event.key == pygame.K_UP:
       change_to = 'UP'
-      if event.key == pygame.K_DOWN:
+    if event.key == pygame.K_DOWN:
         change_to = 'DOWN'
-        if event.key == pygame.K_LEFT:
+    if event.key == pygame.K_LEFT:
           change_to = 'LEFT'
-          if event.key == pygame.K_RIGHT:
+    if event.key == pygame.K_RIGHT:
             change_to = 'RIGHT'
 
 #Prevent snake from moving two directions simultaneously
 if change_to == 'UP' and direction != 'DOWN':
-  direction == 'UP
+  direction = 'UP'
 if change_to == 'DOWN' and direction != 'UP':
-  direction == 'DOWN'
+  direction = 'DOWN'
 if change_to == 'LEFT' and direction != 'RIGHT':
-  direction == 'LEFT'
-if change_to == 'RIGHT' and direction != 'LEFT:
-  direction == 'RIGHT'
+  direction = 'LEFT'
+if change_to == 'RIGHT' and direction != 'LEFT':
+  direction = 'RIGHT'
 
 #Moving snake
 if direction == 'UP':
@@ -108,8 +90,8 @@ if direction == 'RIGHT':
 #Incrementation by 10
 snake_body.insert(0, list(snake_position))
 if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
-  score += 10
-  fruit_spawn = False
+    score += 10
+    fruit_spawn = False
 else:
   snake_body.pop()
 
@@ -117,11 +99,11 @@ if not fruit_spawn:
   fruit_position = [random.randrange(1, (window_x//10)) * 10, random.randrange(1, (window_y//10)) * 10]
 
 fruit_spawn = True
-game_spawn.fill(black)
+game_window.fill(black)
 
 for pos in snake_body: 
-  pygame.draw_rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
-  pygame.draw_rect(game_window, white, pygame.Rect(fruit_position[0], fruit_position[1], 10, 10))
+  pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
+  pygame.draw.rect(game_window, white, pygame.Rect(fruit_position[0], fruit_position[1], 10, 10))
 
 #Game Over condition
 if snake_position[0] < 0 or snake_position[0] > window_x-10:
@@ -135,4 +117,3 @@ for block in snake_body[1:]:
 show_score(1, white, 'times new roman', 20)
 
 pygame.display.update()
-
